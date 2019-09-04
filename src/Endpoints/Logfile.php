02947e6bc772519d6c9874d454c3dd22cd626b26
Tom Kriek <tom@inktweb.nl>
2019-09-04 15:10:08 +0200

    Docs for autocompletion

---
+++ src/Endpoints/Logfile.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TomKriek\CopernicaAPI\Endpoints;
+
+use TomKriek\CopernicaAPI\CopernicaAPI;
+
+/**
+ * Class Database
+ * @package TomKriek\CopernicaAPI\Endpoints
+ *
+ * @method CopernicaAPI csv(array $parameters = [])
+ * @method CopernicaAPI json(array $parameters = [])
+ * @method CopernicaAPI xml(array $parameters = [])
+ */
+class Logfile extends AbstractEndpoint
+{
+
+}
--- src/Endpoints/Logfiles.php
+++ src/Endpoints/Logfiles.php
@@ -2,15 +2,9 @@
 
 namespace TomKriek\CopernicaAPI\Endpoints;
 
-use TomKriek\CopernicaAPI\CopernicaAPI;
-
 /**
  * Class Database
  * @package TomKriek\CopernicaAPI\Endpoints
- *
- * @method CopernicaAPI json(array $parameters = [])
- * @method CopernicaAPI xml(array $parameters = [])
- * @method CopernicaAPI header(array $parameters = [])
  */
 class Logfiles extends AbstractEndpoint
 {
