b8b071798601e327b2bb9737cf12f94538296f8f
Tom Kriek <tom@inktweb.nl>
2019-09-04 14:51:36 +0200

    Missing endpoints

---
+++ src/Endpoints/Condition.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TomKriek\CopernicaAPI\Endpoints;
+
+/**
+ * Class Collection
+ * @package TomKriek\CopernicaAPI\Endpoints
+ */
+class Condition extends AbstractEndpoint
+{
+    // @todo overwrite behaviour here for two parameters sequentially in the uri
+}
---
+++ src/Endpoints/Ms.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TomKriek\CopernicaAPI\Endpoints;
+
+/**
+ * Class Collection
+ * @package TomKriek\CopernicaAPI\Endpoints
+ */
+class Ms extends AbstractEndpoint
+{
+    // @todo overwrite behaviour here for two parameters sequentially in the uri
+}
